4f9a554ed67592a94893de8f0d97892a4edb5d23
CITRON_001 <mrsonsare@gmail.com>
2024-01-11 21:48:57 +0530

    first commit

---
+++ wificonnection.ino
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  // put your setup code here, to run once:
+WiFi.begin("My_Station","myhome@123");
+Serial.begin(9600);
+while(WiFi.status() != WL_CONNECTED)
+{
+  Serial.print("..");
+  delay(200);
+}
+Serial.println();
+Serial.println("NodeMCU is Connected !");
+
+}
+
+void loop() {
+  
+}
